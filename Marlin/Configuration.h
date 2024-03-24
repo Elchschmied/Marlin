dd042e35d686f48eae34b68c00942691ee8270eb
Elchschmied <klaus@korte-gm.de>
2024-03-24 13:24:18 +0100

    Maschine
    Maschine umbenannt

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -119,7 +119,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-//#define CUSTOM_MACHINE_NAME "Ender 3 pro DExt."
+#define CUSTOM_MACHINE_NAME "Ender 3 pro DExt. Marlon"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
